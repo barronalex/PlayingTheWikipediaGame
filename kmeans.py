2a19457ae35f95fa27ffb24d27c62e78e1879352
Zack <zdfs1121@gmail.com>
2015-12-05 14:01:54 -0800

    fuuuuck

--- kmeans.py
+++ kmeans.py
@@ -5,16 +5,7 @@ import sklearn
 def runkmeans_sklearn(examples, K, maxIters):
     kmeans = sklearn.cluster.KMeans(8, init='k-means++', n_init=5)
 
-
 def runkmeans(examples, K, maxIters):
-    '''
-    examples: list of examples, each example is a string-to-double dict representing a sparse vector.
-    K: number of desired clusters. Assume that 0 < K <= |examples|.
-    maxIters: maximum number of iterations to run for (you should terminate early if the algorithm converges).
-    Return: (length K list of cluster centroids,
-            list of assignments, (i.e. if examples[i] belongs to centers[j], then assignments[i] = j)
-            final reconstruction loss)
-    '''
     def increment(d1, scale, d2):
         for f, v in d2.items():
             d1[f] = d1.get(f, 0) + v * scale
