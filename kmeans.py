56ff4f7dbe512d6c580037acab4299c74fb92bcc
Alex Barron <admb@stanford.edu>
2015-12-05 21:39:40 -0800

    Added pickling to kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,12 @@
 import random
+import cPickle
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cluster import KMeans
 
 NUM_K_VALUES = 10
 SPACING = 2
+KMEANS_PICKLE_FNAME = "kmeans_results.pickle"
 
 def runkmeans_sklearn(examples):
     d = DictVectorizer()
@@ -16,6 +18,7 @@ def runkmeans_sklearn(examples):
         kmeans = KMeans(SPACING*k, init='k-means++', n_init=5, verbose=True)
         km = kmeans.fit(X)
         results[SPACING*k] = km
+    cPickle.dump(results, open(KMEANS_PICKLE_FNAME, 'wb'))
     return results
 
 
