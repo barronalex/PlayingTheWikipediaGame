1ea7157f27378ac25dc6b895e9beb33364f64070
Alex Barron <admb@stanford.edu>
2015-12-05 21:00:19 -0800

    Try a bunch of k values in kmeans

--- kmeans.py
+++ kmeans.py
@@ -3,12 +3,19 @@ import sklearn
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cluster import KMeans
 
+NUM_K_VALUES = 10
+SPACING = 2
+
 def runkmeans_sklearn(examples, K):
-    kmeans = KMeans(K, init='k-means++', n_init=1, verbose=True)
     d = DictVectorizer()
     X = d.fit_transform(examples)
     print "sparse matrix created"
-    kmeans.fit(X)
+    results = {}
+    for k in range(1,NUM_K_VALUES + 1):
+        kmeans = KMeans(SPACING*k, init='k-means++', n_init=5, verbose=True)
+        km = kmeans.fit(X)
+        results[SPACING*k] = km
+    return results
 
 
 def runkmeans(examples, K, maxIters):
